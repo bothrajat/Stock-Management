ec03ef76f6147bd5e7cf0d0bedf24971b1769a56
bothrajat <20ucs157@lnmiit.ac.in>
2022-12-30 20:38:13 +0530

    updatemodels

---
+++ myapp/management/commands/updatemodels.py
@@ -0,0 +1,40 @@
+from django.core.management.base import BaseCommand
+import pandas as pd
+from management.models import *
+
+
+
+
+class Command(BaseCommand):
+    help = 'import booms'
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        df = pd.read_csv('customers.csv')
+        for cust in zip(df.Customers):
+            model =Customer(CustomerName = cust)
+            model.save()
+
+        df1 = pd.read_csv('quality.csv')
+        for qual in zip(df1.Quality):
+            model =Quality(Quality= qual)
+            model.save()
+
+        df2 = pd.read_csv('colour.csv')
+        for colo in zip(df2.Colour):
+            model = Colour(Colour=colo)
+            model.save()
+
+        df3 = pd.read_csv('order.csv')
+        for ord in zip(df3.Order):
+            model = Order(OrderNo = ord)
+            model.save()
+
+        df4 = pd.read_csv('sno.csv')
+        for cust, ord, qual, date in zip(df4.Customer, df4.Order, df4.Quality, df4.Date):
+            model = SerialNo(Customer=Customer.objects.get(CustomerName = cust), Order = Order.objects.get(OrderNo=ord), Quality=Quality.objects.get(Quality=qual), Date = date)
+            model.save()
+
+        
